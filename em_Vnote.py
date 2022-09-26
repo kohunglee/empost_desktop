a3b2a10c13f300eb0d5d604d109ecb5316a121ed
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-09-26 16:18:49 +0800

    发笔记和配置功能完成，V 1.0 基本完工

--- em_Vnote.py
+++ em_Vnote.py
@@ -3,6 +3,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+from ast import Delete
 from cgitb import handler
 import webbrowser
 import ipaddress
@@ -13,7 +14,89 @@ import tkinter.messagebox as tm
 import em_module as em
 import em_req
 
+# 配置窗口的生成
+def create_setting(event):
+    top = tk.Toplevel()
+    top.title("emPost 配置")
 
+    width = 200
+    height = 170
+    screenwidth = top.winfo_screenwidth()
+    screenheight = top.winfo_screenheight()
+    size_geo = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2 - 100)
+    top.geometry(size_geo)
+    top.resizable(0,0) # 窗口不能调大小
+
+    em.reConfig()
+    apikey = em.apikey
+    url = em.url
+    if apikey == '':
+        url = ''
+
+    def saveInfo():
+        if em.save_config(akEText.get(), urlEText.get()) == "true" :
+            tm.showinfo("", "保存成功！请自行重启软件以应用")
+        else:
+            tm.showinfo("", "保存失败!")
+
+    msg1 = tk.Label(top, text="API 秘钥：",font=('宋体',14))
+    msg1.pack(anchor="w",padx="10px")
+
+    akE = tk.Entry(top)
+    akEText = tk.StringVar()
+    akE["textvariable"] = akEText
+    akE.pack(padx="10px", pady="5px")
+    akE.focus()
+    akE.insert(0,apikey)
+
+    msg2 = tk.Label(top, text="站点（加 https:// ）：",font=('宋体',14))
+    msg2.pack(anchor="w",padx="10px")
+
+    urlE = tk.Entry(top)
+    urlEText = tk.StringVar()
+    urlE["textvariable"] = urlEText
+    urlE.pack(padx="10px", pady="5px")
+    urlE.insert(0,url)
+
+    # 按钮 
+    saveButton = tk.Button(top, text="保存配置",  command=saveInfo)
+    saveButton.pack(pady = "10px")
+
+# 发文窗口的生成
+def create_SendArticle(event):
+    top2 = tk.Toplevel()
+    top2.title("发文")
+
+    width = 300
+    height = 200
+    screenwidth = top2.winfo_screenwidth()
+    screenheight = top2.winfo_screenheight()
+    size_geo = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2 - 100)
+
+    top2.geometry(size_geo)
+
+    msg = tk.Label(top2, text="下一版敬请期待！",bg='#9BCD9B',font=('宋体',15))
+
+    msg.pack()
+
+# 阅读界面的生成
+def create_Reading(event):
+    top3 = tk.Toplevel()
+    top3.title("读文")
+
+    width = 300
+    height = 200
+    screenwidth = top3.winfo_screenwidth()
+    screenheight = top3.winfo_screenheight()
+    size_geo = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2 - 100)
+
+    top3.geometry(size_geo)
+
+    msg = tk.Label(top3, text="下一版敬请期待！！",bg='#9BCD9B',font=('宋体',15))
+
+    msg.pack()
+
+# 发笔记界面的生成（也是程序主窗口）
 def note_view():
     
     # 定义窗口
@@ -33,40 +116,23 @@ def note_view():
     def note_callback():
         try:
             noteText = text.get("1.0","end")
-            print(len(noteText))
+            # print(len(noteText))
             if len(noteText) <= 1 :
                 tm.showinfo("", "请填写内容！")
                 return
             res = em_req.req("note_post", em.note(noteText))
-            topMsg.set(res)
         except :
             tm.showinfo("", "无法发送！网络问题或程序配置错误！")
             return
 
         if eval(res)["msg"] == "ok":
             tm.showinfo("", "发表成功!")
+            text.delete(0.0, tk.END)
         else:
             tm.showinfo("", "发送失败！请检查配置信息！")
 
-        topMsg.set(res)
+        # topMsg.set(res)
         return
-    
-    # 创造配置窗口
-    def create_setting(event):
-        top = tk.Toplevel()
-        top.title("emPost 配置")
-
-        width = 300
-        height = 200
-        screenwidth = window.winfo_screenwidth()
-        screenheight = window.winfo_screenheight()
-        size_geo = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2 - 100)
-
-        top.geometry(size_geo)
-
-        msg = tk.Label(top, text="网址：c.biancheng.net",bg='#9BCD9B',font=('宋体',15))
-
-        msg.pack() 
 
     # 笨办法、变红、变黑
     def change_color1(event):
@@ -77,9 +143,25 @@ def note_view():
         lSetting.config(fg ="black")
         return
 
+    def change_color3(event):
+        lSendArticle.config(fg ="red")
+        return
+
+    def change_color4(event):
+        lSendArticle.config(fg ="black")
+        return
+
+    def change_color5(event):
+        lReading.config(fg ="red")
+        return
+
+    def change_color6(event):
+        lReading.config(fg ="black")
+        return
+
     # 打开网址
     def open_url(event):
-        webbrowser.open(em.url)
+        webbrowser.open(em.url + '/admin/twitter.php')
 
     # 动态文字
     topMsg = tk.StringVar()
@@ -92,6 +174,7 @@ def note_view():
     text = tk.Text(window, width=50, font = tf.Font(size=16), fg ="#5a5c69", height=10,
                    padx = 5,pady = 5, undo=True, autoseparators=False,borderwidth = "0px")
     text.pack(padx = "10px",fill = tk.BOTH,expand=tk.TRUE)
+    text.focus()
     # text.insert(tk.INSERT, 'some text...')
 
     # 按钮 
@@ -103,14 +186,26 @@ def note_view():
     bLink.pack()
 
     frame_left = tk.Frame(bLink)
+    
     lSetting = tk.Label(frame_left, text='配置', fg ="black")
     lSetting.bind('<ButtonPress-1>',create_setting)
     lSetting.bind('<Enter>',change_color1)
     lSetting.bind('<Leave>',change_color2)
     lSetting.grid(row = 0, column=0)
+
+
+    lSendArticle = tk.Label(frame_left, text='发文', fg ="black")
+    lSendArticle.bind('<ButtonPress-1>',create_SendArticle)
+    lSendArticle.bind('<Enter>',change_color3)
+    lSendArticle.bind('<Leave>',change_color4)
+    lSendArticle.grid(row = 0, column=1)
+
+    lReading = tk.Label(frame_left, text='读文', fg ="black")
+    lReading.bind('<ButtonPress-1>',create_Reading)
+    lReading.bind('<Enter>',change_color5)
+    lReading.bind('<Leave>',change_color6)
+    lReading.grid(row = 0, column=2)
     
-    tk.Label(frame_left,text='发文').grid(row = 0, column =1)
-    tk.Label(frame_left,text='读文').grid(row = 0, column =3)
     frame_left.pack()
 
     window.mainloop()
